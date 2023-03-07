d9c7d702ab74bfaab957f94e3877d2c84826d057
Dan <miradd@bcd07465bab0.ant.amazon.com>
2023-03-07 12:57:47 +0400

    added trimming

--- src/App.js
+++ src/App.js
@@ -11,7 +11,19 @@ function App() {
   return (
     <div className="App">
       <HeroLayout3 />
-<NewForm1 />
+<NewForm1
+    onSubmit={(fields) => {
+        // Example function to trim all string inputs
+        const updatedFields = {}
+        Object.keys(fields).forEach(key => {
+            if (typeof fields[key] === 'string') {
+                updatedFields[key] = fields[key].trim()
+            } else {
+                updatedFields[key] = fields[key]
+            }
+        })
+        return updatedFields
+    }} />
     </div>
   );
 }
